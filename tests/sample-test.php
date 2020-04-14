178990355c9f54b78900793db1840e0ec97c7bdb
Rodrigo <=>
2020-04-14 16:39:59 -0300

    k

--- tests/sample-test.php
+++ tests/sample-test.php
@@ -12,42 +12,62 @@ echo "---------------------------";
 echo "Creando Driver de navegador";
 echo "---------------------------";
 
-$web_driver = RemoteWebDriver::create(
-  $selenium_server,
-  \Facebook\WebDriver\Remote\DesiredCapabilities::chrome()
-  , 120000
-);
+try {
+  $web_driver = RemoteWebDriver::create(
+    $selenium_server,
+    \Facebook\WebDriver\Remote\DesiredCapabilities::chrome()
+    , 120000
+  );
+catch(Exception $e) {
+  echo 'Error creando el WebDriver --- \n' .$e->getMessage();
+}
 
 echo "---------------------------";
 echo "Obteniendo datos de URL";
 echo "---------------------------";
 
-$web_driver->get($test_url);
+try {
+  $web_driver->get($test_url);
+catch(Exception $e) {
+  echo 'Error obteniendo datos de la url --- \n' .$e->getMessage();
+}
 
 echo "---------------------------";
 echo "Obteniendo elementos HTML";
 echo "---------------------------";
 
-$element_username = $web_driver->findElement(WebDriverBy::id("edit-name"));
-$element_pass = $web_driver->findElement(WebDriverBy::id("edit-pass"));
-$element_submit = $web_driver->findElement(WebDriverBy::id("edit-submit"));
+try {
+  $element_username = $web_driver->findElement(WebDriverBy::id("edit-name"));
+  $element_pass = $web_driver->findElement(WebDriverBy::id("edit-pass"));
+  $element_submit = $web_driver->findElement(WebDriverBy::id("edit-submit"));
+catch(Exception $e) {
+  echo 'Error obteniendo elementos del html --- \n' .$e->getMessage();
+}
 
 echo "---------------------------";
 echo "Rellenando datos del formulario";
 echo "---------------------------";
 
-if($element_username && $element_pass) {
-  $element_username->sendKeys("admin");
-  $element_pass->sendKeys("admin");
-
-  $element_submit->submit();
+try {
+  if($element_username && $element_pass) {
+    $element_username->sendKeys("admin");
+    $element_pass->sendKeys("admin");
+  
+    $element_submit->submit();
+  }
+catch(Exception $e) {
+  echo 'Error rellenando el formulario de login --- \n' .$e->getMessage();
 }
 
 echo "---------------------------";
 echo "Haciendo print de titulo obtenido";
 echo "---------------------------";
 
-print $web_driver->getTitle();
+try {
+  print $web_driver->getTitle();
+catch(Exception $e) {
+  echo 'Error obteniendo el titulo --- \n' .$e->getMessage();
+}
 
 echo "---------------------------";
 echo "Terminando";
